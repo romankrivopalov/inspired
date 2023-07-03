9afa7fd33097662a11f6858fe588da183ba3c77c
Work desktop <the.red.lemon@yandex.ru>
2023-07-03 13:52:19 +0300

    add initial files

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import './index.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Привет React</h1>
+  </React.StrictMode>,
+)
