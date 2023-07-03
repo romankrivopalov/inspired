0fdb1199d6a01955238f3a42db794f85e581d313
Work desktop <the.red.lemon@yandex.ru>
2023-07-03 14:54:00 +0300

    creates a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,7 @@
+const App = () => (
+  <div>
+    <h1>Привет React</h1>
+  </div>
+)
 
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.scss';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Привет React</h1>
+    <App />
   </React.StrictMode>,
 )
