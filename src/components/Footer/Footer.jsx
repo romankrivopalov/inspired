4ac874a6258a76cf6a08f6bc9b7476c969cd8b7e
Home <home@gmail.com>
2023-07-04 21:50:17 +0300

    adds a link to the designer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => (
       <div className={s.development}>
         <div className={s.developmentList}>
           <p>
-            Designer: <a className={s.link} href="/">Anastasia Ilina</a>
+            Designer: <a className={s.link} href="https://t.me/Mrshmallowww">Anastasia Ilina</a>
           </p>
           <p>
             Developer: <a className={s.link} href="https://github.com/romankrivopalov">Roman Krivopalov</a>
