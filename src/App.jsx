be3bcba8a475fb09bea6a9299b67bfbad0da23bc
Home <home@gmail.com>
2023-07-04 20:58:15 +0300

    uses createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Header from './components/Header/Header.jsx';
-import Footer from './components/Footer/Footer.jsx';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Root from './routes/Root.jsx';
+import MainPage from './components/MainPage/MainPage.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
 
-const App = () => (
-  <>
-    <Header />
-    <Footer />
-  </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<MainPage />} />
+      <Route path="women" element={<MainPage gender="women" />} />
+      <Route path="men" element={<MainPage gender="men" />} />
+      <Route path="woman/:category" element={<MainPage gender="women" />} />
+      <Route path="men/:category" element={<MainPage gender="men" />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Route>
+  )
 )
+
+const App = () => <RouterProvider router={router}></RouterProvider>
 
 export default App;
