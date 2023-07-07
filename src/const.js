36ef629b7921ea782fdddb9a50cea16326e880b5
Work desktop <the.red.lemon@yandex.ru>
2023-07-07 09:55:03 +0300

    adds constants for api

---
+++ src/const.js
@@ -0,0 +1,4 @@
+export const API_URL = 'http://localhost:8024';
+export const GOODS_URL = `${API_URL}/api/goods`; // GET // получить список всех товаров с пагинацией
+export const CATEGORY_URL = `${API_URL}/api/categories`; // GET // получить список категорий
+export const COLORS_URL = `${API_URL}/api/colors`; // GET // получить список цветов
