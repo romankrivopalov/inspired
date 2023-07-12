b923cfd2a6d78c0271d1c00e4032f841fa928590
Work desktop <the.red.lemon@yandex.ru>
2023-07-12 15:58:42 +0300

    refactor, adds categoryData

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -9,12 +9,18 @@ import Banner from "../Banner/Banner.jsx";
 const MainPage = () => {
   const { gender, category } = useParams(),
         dispatch = useDispatch(),
-        { activeGender, categories } = useSelector(state => state.navigation),
-        genderData = categories[activeGender];
+        { activeGender, categories, genderList } = useSelector(state => state.navigation),
+        genderData = categories[activeGender],
+        categoryData = genderData?.list.find(item => item.slug === category);
 
   useEffect(() => {
-    dispatch(setActiveGender(gender));
-  }, [gender, dispatch])
+    if (gender) {
+      dispatch(setActiveGender(gender));
+    } else if (genderList[0]) {
+      dispatch(setActiveGender(genderList[0]));
+      dispatch(fetchGender(genderList[0]));
+    }
+  }, [gender, genderList, dispatch])
 
   useEffect(() => {
     if (gender && category) {
@@ -32,7 +38,7 @@ const MainPage = () => {
     <>
       <Banner data={genderData?.banner} />
       <Goods
-        categoryData={genderData?.list.find(item => item.slug === category)} />
+        categoryData={categoryData} />
     </>
   )
 }
